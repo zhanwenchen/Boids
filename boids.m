a7bd7b034e1810b1dc73b78a30a33c79533000a3
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2016-12-11 18:48:18 -0500

    implemented collision detection

--- boids.m
+++ boids.m
@@ -2,26 +2,18 @@
 %% CS250
 %% Final Project
 
-numBirds = 2;
-
 %% Bird vars
-max_speed = 20; %m/s
+max_speed = 10; %m/s
+numBirds = 2;
+side_length = 10;
 
 %% Randomization funcs
 rand_sign = @(n) ones(n, 1) - floor(rand(n,1)*2)*2; % pick -1 or 1, n by 1
 rand_speed = @(n) (rand(n,1) - 0.5) * 2 * max_speed; % [-max_speed, max_speed]
 
-
-
 %% Environment vars
 sky_xlim = 200;
 sky_ylim = 200;
-
-% dx = @(birds) (rand(numBirds,1).*2 - 1) .* max_speed; % dx in [-max_speed, max_speed]
-% % dy = @(birds) rand_sign(:,2) .* sqrt(max_speed^2 - birds(:,3).^2); % pos or neg
-dy = @(birds) rand_sign(:,2) .* sqrt(abs(max_speed^2 - birds(:,3).^2)); % pos or neg
-% % dBird = [dx, dy, dv_x, dv_y]
-% dBird = @(bird) [dx(bird) dy(bird) ones(numBirds,1) ones(numBirds,1)];
 
 %% Init birds
 birds = ones(numBirds, 4); % bird has [x,y,v_x,v_y],
@@ -30,20 +22,15 @@ birds(:,2) = birds(:,2) .* (rand(numBirds,1) * sky_ylim);
 birds(:,3) = rand_sign(numBirds) .* rand_speed(numBirds);
 birds(:,4) = rand_sign(numBirds) .* rand_speed(numBirds);
 
-
-birdList = mat2cell(birds,ones(numBirds,1),4); % same thing as birds
+% birdList = mat2cell(birds,ones(numBirds,1),4); % same thing as birds
 birdsList{1} = birds; % birds by frame
 
 numIterations = 200; %s
 
 for i=2:numIterations
   %% Collision
-  % for all other birds
-  % if
-  % birdsList{i} = birdsList{i-1}...
-  %               + dBirds(birds(:,1),birds(:,2),birds(:,3),birds(:,4));
-  % birds = birds + dBirds();
-  birds = birds + dBirds(birds);
+  birds = birds + dBirds(birds,side_length,max_speed);
   birdsList{i} = birds;
-
 end
+
+viz(birdsList,1,side_length)
--- viz.m
+++ viz.m
@@ -1,32 +1,31 @@
 %% viz(birds, interval)
 % --------------------
 % Input: birds, a
-function [] = viz(birdsList,interval)
+function [] = viz(birdsList,interval,side_length)
 
 numBirds = size(birdsList{1},1);
 
 % Check number of inputs.
-if nargin == 1
-    interval = 1;
-end
-
-side_length = 1;
+% if nargin == 1
+%     interval = 1;
+%     side_length = 2;
+% end
 
   for i=1:interval:length(birdsList)
     %% draw all birds in frame i
     for j=1:numBirds
-      fprintf('Drawing \n')
       rectangle('Position',...
-                [birdsList{i}(j,1),...
-                 birdsList{i}(j,2),...
+                [birdsList{i}(j,1) - 0.5 * side_length,...
+                 birdsList{i}(j,2) - 0.5 * side_length,...
                  side_length,...
                  side_length]);
     end
     title(sprintf('Frame: %d',i));
     xlim([0,200]);
     ylim([0,200]);
+    % axis equal;
 
-    fprintf('Waiting for any key to be pressed\n');
+    % fprintf('Waiting for any key to be pressed\n');
     w = waitforbuttonpress;
     clf;
   end
